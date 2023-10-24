83214a9f8ca80a0c2767c90e79159eca828d2880
Julz Molina <jmolina.dex@gmail.com>
2023-10-24 16:08:16 -0700

    Storybook ready

---
+++ docs/assets/preview-dbd751e6.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-1a49707e.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-d3b23376.js"),["./DocsRenderer-NNNQARDV-d3b23376.js","./iframe-1a49707e.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./react-18-063a39db.js","./index-932496f1.js","./index-22d400a1.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-dbd751e6.js.map
